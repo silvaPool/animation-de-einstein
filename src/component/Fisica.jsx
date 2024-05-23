74bf6110afdcd09404c38d2477e126cde88ca601
silvaPool <silva.friends22@gmail.com>
2024-05-23 11:12:21 -0300

    Gancho useTransition

--- src/component/Fisica.jsx
+++ src/component/Fisica.jsx
@@ -1,32 +1,52 @@
-import { useSpring, animated } from "@react-spring/web";
+import { useSpring, animated, useTransition } from "@react-spring/web";
 import { useState } from "react";
 
 export default function Fisica() {
-  const [isVisible, setIsVisible] = useState(false);
+//   const [isVisible, setIsVisible] = useState(false);
 
-  const opacityConfig = {
-    tension: 300,
-    friction: 40,
-  };
+//   const opacityConfig = {
+//     tension: 300,
+//     friction: 40,
+//   };
 
-  const opacityAnimation = useSpring({
-    opacity: isVisible ? 1 : 0,
-    config: opacityConfig,
-  });
+//   const opacityAnimation = useSpring({
+//     opacity: isVisible ? 1 : 0,
+//     config: opacityConfig,
+//   });
 
-  const toggleVisibility = () => setIsVisible(!isVisible);
+//   const toggleVisibility = () => setIsVisible(!isVisible);
+
+    const [items, setItems] = useState([]);
+
+    const addItem = () => {
+        const newItem = `Item ${items.length + 1}`;
+        setItems([...items, newItem]);
+    }
+
+    const removeItem = () => {
+        if (items.length === 0) return;
+        const newItems = items.slice(0, -1);
+        setItems(newItems);
+    };
+
+    const transitions = useTransition(items, {
+        from: {opacity: 0, transform: "translate3d(0, -40px, 0)" },
+        enter: {opacity: 1, transform: "translate3d(0, 0, 0)"},
+        leave: {opacity: 0, transform: "translate3d(0, -40px, 0)"},
+    });
 
   return (
-    <div>
-      <button
-        onClick={toggleVisibility}
-        aria-label={isVisible ? "Hide" : "Show"}
-      >
-        {isVisible ? "Hide" : "Show"}
-      </button>
-      <animated.div style={opacityAnimation}>
-        This text will fade in and out with spring physics.
-      </animated.div>
+    <div className="transitionDiv">
+        <div>
+            <button onClick={addItem}>Add Item</button>
+            <button onClick={removeItem}>Remove Item</button>
+        </div>
+
+        <div className="transitionItem">
+            {transitions((style, item) => {
+                <animated.div style={style} className='list'>{item}</animated.div>
+            })}
+        </div>
     </div>
   );
 }
